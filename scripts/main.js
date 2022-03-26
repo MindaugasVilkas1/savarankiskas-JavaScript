6acdb2a1e4d01fee38e24fc7628dd64a9b312a43
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-03-26 12:41:45 +0200

    Isvestos prekes is json failo

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,25 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("show")
 })
+
+fetch("http://localhost:3000/Items")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(element => getItems(element))
+    })
+
+
+
+
+
+let getItems = (item) => {
+    document.querySelector("#items").innerHTML += `
+    <div>
+        <h3>${item.title}</h3>
+        <img src="${item.image}"><br>
+        <span>Kaina: ${item.price}</span><br>
+        <span>Dydis: ${item.size}. ${item.about}</span>
+    </div>
+    `
+}
