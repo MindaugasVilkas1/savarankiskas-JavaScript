9b9dcb686b56403e21c5287bbb4b94757d160429
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-03-26 18:17:25 +0200

    sukurta forma index html bei post metodas main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import Items from "./class.js";
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
@@ -13,14 +14,33 @@ fetch("http://localhost:3000/Items")
 
 
 
+.catch(err => console.log(err, "Įvyko klaida, patikrinkite duomenis"))
+document.querySelector("#form").addEventListener("submit", e => {
+    e.preventDefault()
+    let title = e.target.elements.title.value;
+    let image = e.target.elements.url.value;
+    let price = e.target.elements.price.value;
+    let size = e.target.elements.size.value;
+    let about = e.target.elements.about.value;
+    let items = new Items(title, image, price, size, about)
+    console.log(items)
+    fetch("http://localhost:3000/Items", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json plain/text "
+        },
+        body: JSON.stringify(items)
+    })
 
+})
 
 let getItems = (item) => {
     document.querySelector("#items").innerHTML += `
-    <div>
+    <div class=allItems>
         <h3>${item.title}</h3>
         <img src="${item.image}"><br>
-        <span>Kaina: ${item.price}</span><br>
+        <span>Kaina: ${item.price}€</span><br>
         <span>Dydis: ${item.size}. ${item.about}</span>
     </div>
     `
