f853c406002367f3702aaea147c7eace2922691f
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-03-26 18:14:05 +0200

    sukurta class.js Items exportuota i main.js

---
+++ scripts/class.js
@@ -0,0 +1,10 @@
+class Items {
+    constructor(title, image, price, size, about) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+        this.size = size;
+        this.about = about
+    }
+}
+export default (Items);
